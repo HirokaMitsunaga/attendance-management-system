45e3799aa0d55ee756af1340fee919b26a9025a0
hiroka <mitsunagahrk87@gmail.com>
2026-01-06 09:25:12 +0900

    feat:勤怠修正で承認された勤怠のユースケースの実装

---
+++ backend/src/command/useCase/attendance-correction/approve-attendance-correction.use-case.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import type { IAttendanceCorrectionRepository } from 'src/command/domain/attendance-correction/attendance-correction-repository.interface';
+import type { IAttendanceRecordRepository } from 'src/command/domain/attendance-record/attendance-record-repository.interface';
+import { AttendanceCorrectionApproval } from 'src/command/domain/attendance-correction/attendance-correction-approval';
+import { EntityId } from 'src/command/domain/entity-id.vo';
+import { NotFoundError } from 'src/common/errors/not-found.error';
+import { formatDateToISOString } from 'src/common/utils/date.utils';
+
+export type ApproveAttendanceCorrectionParams = {
+  userId: string;
+  workDate: Date;
+  approvedBy: string;
+  approveAt: Date;
+};
+
+@Injectable()
+export class ApproveAttendanceCorrectionUseCase {
+  constructor(
+    private readonly attendanceCorrectionRepository: IAttendanceCorrectionRepository,
+    private readonly attendanceRecordRepository: IAttendanceRecordRepository,
+  ) {}
+
+  async execute(params: ApproveAttendanceCorrectionParams): Promise<void> {
+    const userId = EntityId.create({ entityId: params.userId });
+
+    const correction =
+      await this.attendanceCorrectionRepository.findByUserIdAndWorkDate({
+        userId: userId.getEntityId(),
+        workDate: params.workDate,
+      });
+
+    if (!correction) {
+      throw new NotFoundError(
+        '勤怠修正',
+        `${params.userId}:${formatDateToISOString(params.workDate)}`,
+      );
+    }
+    correction.approve({
+      approvedBy: params.approvedBy,
+      approvedAt: params.approveAt,
+    });
+
+    //TODO:勤怠集約の追加と同一トランザクションにするのか検討する
+    await this.attendanceCorrectionRepository.save({ correction });
+
+    //承認された勤怠の追加
+    const record =
+      await this.attendanceRecordRepository.findByUserIdAndWorkDate({
+        userId,
+        workDate: params.workDate,
+      });
+    if (!record) {
+      throw new NotFoundError(
+        '勤怠記録',
+        `${params.userId}:${formatDateToISOString(params.workDate)}`,
+      );
+    }
+
+    new AttendanceCorrectionApproval().applyApprovedPunchEventsToRecord({
+      record,
+      approvedPunchEvents: correction.getApprovedPunchEvents(),
+    });
+
+    //TODO:修正集約のと同一トランザクションにするのか検討する
+    await this.attendanceRecordRepository.save({ record });
+  }
+}
