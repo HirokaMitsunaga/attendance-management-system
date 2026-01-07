b83fd5fdd3f8e2b05cb4265cbcc5eaa46ac01b41
hiroka <mitsunagahrk87@gmail.com>
2026-01-08 08:26:29 +0900

    fix:エラーメッセージを定数化

--- backend/src/command/useCase/attendance-correction/resubmit/resubmit-clock-in-attendance-correction.use-case.ts
+++ backend/src/command/useCase/attendance-correction/resubmit/resubmit-clock-in-attendance-correction.use-case.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import type { IAttendanceCorrectionRepository } from 'src/command/domain/attendance-correction/attendance-correction-repository.interface';
 import { PUNCH_TYPE } from 'src/command/domain/common/punch/punch-type';
 import { EntityId } from 'src/command/domain/entity-id.vo';
+import { ATTENDANCE_CORRECTION } from 'src/common/constants';
 import { NotFoundError } from 'src/common/errors/not-found.error';
 import {
   getCurrentDate,
@@ -35,7 +36,7 @@ export class ResubmitClockInAttendanceCorrectionUseCase {
 
     if (!correction) {
       throw new NotFoundError(
-        '勤怠修正',
+        ATTENDANCE_CORRECTION.NOT_FOUND,
         `${params.userId}:${formatDateToISOString(params.workDate)}`,
       );
     }
