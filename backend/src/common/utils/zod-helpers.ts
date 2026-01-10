bdfdad1789a266e5063c758b6c1a9d43bad37385
hiroka <mitsunagahrk87@gmail.com>
2026-01-10 17:33:41 +0900

    feat:zodでのdate変換の処理をutiltsへ記載

---
+++ backend/src/common/utils/zod-helpers.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { INVALID_FORMAT, REQUIRED_FIELD } from '../constants';
+import { parseISOString } from './date.utils';
+
+/**
+ * JSON文字列からDateオブジェクトへの変換とバリデーションを行うZodスキーマ
+ * 勤怠記録の日付フィールド（勤務日、打刻日時など）で共通利用
+ *
+ * @param fieldLabel - エラーメッセージに表示するフィールド名
+ * @returns Zodスキーマ
+ */
+export const dateFromJsonSchema = (fieldLabel: string) =>
+  z
+    .preprocess(
+      (value) => (typeof value === 'string' ? parseISOString(value) : value),
+      z.date({
+        required_error: REQUIRED_FIELD(fieldLabel),
+        invalid_type_error: INVALID_FORMAT(fieldLabel),
+      }),
+    )
+    .refine((date) => !Number.isNaN(date.getTime()), {
+      message: INVALID_FORMAT(fieldLabel),
+    });
